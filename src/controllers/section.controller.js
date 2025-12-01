16f30b631ea57b6fb3ec05bb4225f80868c8d4fb
Youssef-Alshwawra <157247477+Youssef-Alshwawra@users.noreply.github.com>
2025-12-02 01:35:45 +0300

    refactor: simplify getAllSections implementation using curdFactory

--- src/controllers/section.controller.js
+++ src/controllers/section.controller.js
@@ -1,13 +1,19 @@
+import { eq } from "drizzle-orm";
 import db from "../config/db.js"
 import { sectionTable } from "../db/schema.js"
 import responseHandler from "../utils/responseHandler.js";
+import { getAll, getById } from "../utils/curdFactory.js";
 
-export const getAllSections = async (req, res) => {
-    const sections = await db.select().from(sectionTable);
+// export const getAllSections = async (req, res) => {
+//     const sections = await db.select().from(sectionTable);
 
-    if(sections.length <= 0) {
-        return responseHandler(res, 200, 'There is no sections', null, false);
-    }
+//     if(sections.length <= 0) {
+//         return responseHandler(res, 200, 'There is no sections', null, false);
+//     }
 
-    return responseHandler(res, 200, 'Sections found successfully', sections);
-}
+//     return responseHandler(res, 200, 'Sections found successfully', sections);
+// }
+
+export const getAllSections = getAll(sectionTable);
+
+export const getSectionById = getById(sectionTable);
