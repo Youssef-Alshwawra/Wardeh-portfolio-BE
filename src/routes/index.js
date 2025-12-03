fe68b7c41d5c41713c1cf297e9c70223124562e7
Youssef-Alshwawra <157247477+Youssef-Alshwawra@users.noreply.github.com>
2025-12-04 01:20:06 +0300

    feat: add authentication routes to the main router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import sectionRoutes from './section.routes.js';
 import subsectionRoutes from './subsection.routes.js';
+import authRoutes from './auth.routes.js';
 
 const router = Router();
 
+router.use('/api/auth', authRoutes);
 router.use('/api/sections', sectionRoutes);
 router.use('/api/subsections', subsectionRoutes);
 
